0995e392f7825e47de1ae2a323a22eda74e3876f
428770 <428770@edu.cwa.ac.uk>
2024-10-04 15:06:01 +0100

    setup stuff

---
+++ env_myTestProject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!\\dc1\UserData\428770\Documents\Django\Week3\env_myTestProject\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
