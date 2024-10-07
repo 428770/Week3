d93a53296ac06ab4d0b7cf588bbd189464c52fd3
428770 <428770@edu.cwa.ac.uk>
2024-10-07 14:04:46 +0100

    just in case

--- testProject/testApp/models.py
+++ testProject/testApp/models.py
@@ -8,3 +8,11 @@ class Pet(models.Model):
     submitter = models.CharField(max_length=100)
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    age = models.IntegerField()
+    submission_date = models.DateTimeField()
+    vaccinations = models.ManyToManyField('Vaccine', blank = True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
