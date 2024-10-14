1db3430c3b48e4a509235d261e0304663a4d7fee
428770 <428770@edu.cwa.ac.uk>
2024-10-14 13:11:08 +0100

    starting new project

--- testProject/testApp/views.py
+++ testProject/testApp/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.shortcuts import render #not used yet need templates first
 from django.http import HttpResponse #testing that url routing is working?
 from .models import Pet
+from django.http import Http404 # needed for 404 response
+
 
 def home(request):
     #perform an orm query to get all pets
@@ -13,5 +15,11 @@ def home(request):
     # 'home.html and pass pets through as an argument in a dictionary
     return render(request,	'home.html', {'pets':pets,})
 
-def pet_detail(request,pet_id):
-    return HttpResponse(F"<h1>Pet Number: {pet_id}") 
+def pet_detail(request, pet_id): 
+    try:
+        # perform an ORM query to get specific pet
+        pet = Pet.objects.get(id=pet_id) 
+    except Pet.DoesNotExist:
+        # Show an appropriate 404 message
+        raise Http404("Pet not found")
+    return render(request, 'pet_detail.html', {'pet':pet,})
